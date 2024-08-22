b482533d521727ea7e96c736f30119acb7e0867c
mohdisasheikh <mdishk002@gmail.com>
2024-08-22 21:22:40 +0530

    some changes in code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,10 +5,10 @@ let flag = 0;
 
 mbtn.addEventListener("click", function () {
   if (flag === 0) {
-    mbox.style.width = "60%";
+    mbox.style.right = "0%";
     flag = 1;
   } else {
-    mbox.style.width = "0%";
+    mbox.style.right = "-60%";
     flag = 0;
   }
 });
